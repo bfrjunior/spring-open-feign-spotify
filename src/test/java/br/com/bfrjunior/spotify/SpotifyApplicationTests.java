32626dd938bd16f0651c564fc875f8d1486efa80
Fernando Rocha <beneditofernandor@gmail.com>
2024-08-17 00:34:19 -0300

    initi

---
+++ src/test/java/br/com/bfrjunior/spotify/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bfrjunior.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
